758c31307cac25ccb7409c18701b16a28e4c4b87
R Abhishek <43127616+abhi111q@users.noreply.github.com>
2019-03-22 22:22:48 +0530

    after sorting
    after sorting of array

--- operatingProject.c
+++ operatingProject.c
@@ -5,23 +5,45 @@
 struct OS{
 	char name[10];
 	int quantity;
+	
 };
 
 int main(){
-	struct OS o[10];
-	int i;
+	struct OS o[10],temp;
+	int j,i;
 	printf("WELCOME TO MY OPERATING SYSTEM PROGRAM \n\n");
-	
+	getchar();
 	for(i=0;i<10;i++){
+		system("cls");
 		printf("\nEnter %dth name :",i+1);
 		scanf("%s",o[i].name);
 		printf("\nEnter %dth quantity number:",i+1);
 		scanf("%d",&o[i].quantity);
 	}
 	system("cls");
-	printf("Entered name and quantity are as follows::");
+	printf("Entered name and quantity are as follows::\n\n");
 	printf("Name \t\t Quantity");
 	for(i=0;i<10;i++){
 		printf("\n%s \t\t %d",o[i].name,o[i].quantity);
 	}
+	
+	printf("\nSorted by quantity number::\n\n");
+	
+	for(i=0;i<10;i++){
+		for(j=0;j<9;j++){
+			if(o[j].quantity<o[j+1].quantity){
+				
+			temp=o[j];
+			o[j]=o[j+1];
+			o[j+1]=temp;	
+			}
+		}
+	}
+	system("cls");
+	printf("After sorting by their quantity::\n\n");
+	printf("Name \t\t Quantity");
+	for(i=0;i<10;i++){
+		printf("\n%s \t\t %d",o[i].name,o[i].quantity);
+	}
+		
 }
