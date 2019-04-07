fc23268d6b00f42aa4a8f40f4bef2a2289e4e060
R Abhishek <43127616+abhi111q@users.noreply.github.com>
2019-04-07 14:15:36 +0530

    final
    final

--- operatingProject.c
+++ operatingProject.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-#include<conio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<windows.h>
+#include<unistd.h>
 
-
-  // made and owned by :- 
+// made and owned by :- 
   
  // ####### #	  # #       000000
  // #     # #     # #      0      0
@@ -14,127 +12,88 @@
  // ##      #	  # #      0      0
  // #  #	#	  # #      0      0
  // #    #   #####  ######  000000
-
-
-
+ 
+  // Submitted to : Pushpendra Sir 
 
 struct OS{
 	char name[10];
 	int quantity;
 	
-}o[10],temp;
+};
 
-// function declaration
-void program();
-void gotoxy(int ,int);
 
-void intro1();
-void intro1()
-{   system("color C0");
- 	Sleep(10);
-	int top=1,left=39,right=80,bottom=6,i;
-        gotoxy(39,0);for(i=0;i<42;i++)printf("%c",178);  //for first horizontal line
-        for(i=-1;i<15;i++)   //for vertical lines
-		{  system("color A0");
-			gotoxy(38,1+i);printf("%c",178);// 1st vertical 
-			gotoxy(39,1+i);printf("%c",178);
-			gotoxy(81,1+i);printf("%c",178); // 2nd vertical 
-	        gotoxy(82,1+i);printf("%c",178);
-		} 
-        for(i=0;i<42;i++)//lower horizontal lines
-		{ system("color B0");
-			gotoxy(i+39,15);printf("%c",178);
-		}
-        gotoxy(2,25);printf(" Made by :-    RULO ABHISHEK ");
-        gotoxy(2,28);for(i=0;i<30;i++)printf("%c",178);
-		gotoxy(85,25);printf("Submitted to :-PUSHPENDRA SIR "); 
-		gotoxy(85,28);for(i=0;i<30;i++)printf("%c",178);
-		
-		for(i=0;i<37;i++)// for inner horizontal design
-		{ system("color C6");
-			gotoxy(42+i,3);printf("%c",035);
-			gotoxy(42+i,12);printf("%c",030);
-		} 
-		for(i=2;i<12;i++)   //for innner vertical lines
-		{ system("color E9");
-			gotoxy(41,1+i);printf("%c",035);
-			gotoxy(79,1+i);printf("%c",035);
-     	} 
-		gotoxy(43,5);printf("      WELCOME TO MY PROJECT");
-		gotoxy(42,9);printf("    LONGEST REMAINING TIME FIRST ");                                 	
-
+void printBill(struct OS s[],int m){
+	
+	char ch;
+	float price;
+	int i,p;
+	printf("\n\n\t\tDo you want to print Bill :");
+	printf("\n\n\t\tEnter '7' for yes and '8' for NO ::");
+	scanf("%d",&p);
+    if(p==7){
+    	system("clear");
+     	printf("\n\n\t\t Enter the price of One gift :");
+     	scanf("%f",&price);
+     	
+		printf("\n\n\t\t\t\t || Bill of every customer In order are  ||");
+		printf("\n\n\n\t\t\t\t ---- \t\t -------- \t  ----- \n ");
+		printf("\t\t\t\t Name \t\t Quantity \t  Price \n ");
+     	printf("\t\t\t\t ---- \t\t -------- \t  ----- \n ");
+     	for(i=0;i<m;i++){
+			 printf("\n\n\t\t\t\t %s \t\t %d \t\t %.2f ",s[i].name,s[i].quantity,s[i].quantity*price);	 
+     	 }
+	 	printf("\n\n\n\n\n\n\n\n\n\t\t Thanks for using my Program : \n\n\t\t Have a Great Day !!");
+	 	sleep(5);
+	 }
+	 else{
+	 	printf("\n\n Thanks for using my Program : \n\n\t\t Else part  !!");
+	 }
 }
-
-void gotoxy(int x,int y)
-{
-	HANDLE Manipulador;
-	COORD Coordenadas;
-	Manipulador=GetStdHandle(STD_OUTPUT_HANDLE);
-	Coordenadas.X=x;
-	Coordenadas.Y=y;
-	SetConsoleCursorPosition(Manipulador,Coordenadas);
-}
-
-
-
-int main(){
-	int st;
-	do{
-		system("cls");
-	printf("\n\n 1. Enter the program \n\n 2. Exit\n\n \t\t Enter your choice :: ->");
-	scanf("%d",&st);
-	switch(st){
-		case 1: system("cls");
-				program();
-				break;
-		case 2:exit(0);
-				break;
-		default:printf("\n You have entered wrong choice\n\n Press any key to continue ::\n\n ");	
-				getch();	
-	}
-	}while(st!=2);
-
-}
-
-
 
 void program(){
-	
+	system("clear");
+	int n;
+	printf("\n\t----------------------\t\t\t\t\t\t\t\t----------------------------\n");
+	printf("\tMade By: Rulo Abhishek  \t\t\t\t\t\t\tSubmitted to: Pushpendra Sir \n");
+	printf("\t----------------------  \t\t\t\t\t\t\t---------------------------- \n");
+	printf("\n\t\t\t\t\t--------------------------------------\n");
+	printf("\n\n\t\t\t\t\tWELCOME TO MY OPERATING SYSTEM PROGRAM \n");
+	printf("\n\n\t\t\t\t\t-------------------------------------- \n\n");
+	printf("\n\n\t\t Enter the Number of Process ::");
+	scanf("%d",&n);
+	struct OS o[n],temp;
 	int j,i;
-	//intro1();
-	getch();
-	system("cls");
-	gotoxy(40,0);
-	printf("WELCOME TO MY OPERATING SYSTEM PROGRAM \n\n");
-	printf("Enter name from \n\n \t a \t b \t c \t d \t e \t f \t g \t h \t i \t j\n\n\n\n\t\t Press any key to continue :: ");
-		getch();
+	system("clear");
+	printf("\n\n\nEnter name from \n\n \t a \t b \t c \t d \t e \t f \t g \t h \t i \t j\n\n\n\n\t\t\t\t    !!!!  Wait for 5 Seconds  !!!! \n\n \t\t\t\t !!!!  Thanks for your Patience  !!!!\n\n");
+	sleep(5);
 		
 	// logic to take input
 		
-	for(i=0;i<10;i++){
-		system("cls");
-		printf("\nEnter number %d  name :",i+1);
+	for(i=0;i<n;i++){
+		system("clear");
+		printf("\n\n\t\tEnter number %d  name :",i+1);
 		scanf("%s",o[i].name);
-		printf("\nEnter number %d quantity number:",i+1);
+		printf("\n\n\t\tEnter number %d quantity number:",i+1);
 		scanf("%d",&o[i].quantity);
-	}
+	}system("clear");
+	printf("\n\n\t\t\t\t Wait for 5 seconds  !!!  Let me Process !!\n\n");
+	sleep(10);
 	
 	// displaying the entered name along with quantity
 	
-	system("cls");
-	printf("\t\tEntered name and quantity are as follows::\n\n");
-	printf("\n\n\t\tName \t\t Gift Quantity\n\n");
-	for(i=0;i<10;i++){
-		printf("\n\n\t\t%s \t\t %d",o[i].name,o[i].quantity);
+	system("clear");
+	printf("\n\n\t\t\t\t    ## Entered\t NAME \t QUANTITY  ##\n\n");
+	printf("\n\n\t\t\t\t---- \t\t\t\t-------------");
+	printf("\n\t\t\t\tName \t\t\t\tGift Quantity\n");
+	printf("\t\t\t\t---- \t\t\t\t-------------\n\n");
+	for(i=0;i<n;i++){
+		printf("\n\n\t\t\t\t%s \t\t\t\t %d",o[i].name,o[i].quantity);
 	}
-	printf("\n\n\t\tPress any key to continue ::\n\n");
-	printf("\n\n\t You will get your sorted list ");
-	getch();
-	//	system("cls");
+	
 	// logic to sort structure array on the basis of quantity
 	
-	for(i=0;i<10;i++){
-		for(j=0;j<9;j++){
+	for(i=0;i<n;i++){
+		for(j=0;j<n-1;j++){
 			if(o[j].quantity<o[j+1].quantity){			
 			temp=o[j];
 			o[j]=o[j+1];
@@ -142,56 +101,49 @@ void program(){
 			}
 		}
 	}
-	
+	printf("\n\n\n\n\t\t\t\t !!!!   Sorting Your process   !!!!\n");
 	// printing after sorting
 	
-
-
-	printf("After sorting by their quantity::\n\n");
-	printf("\t\tName \t\t Quantity");
-	for(i=0;i<10;i++){
-		printf("\n\n\t\t%s \t\t %d",o[i].name,o[i].quantity);
+	sleep(5);
+	system("clear");
+	
+	printf("\n\t\t\t  ####  After sorting by their quantity  ####\n\n");
+	printf("\t\t\t\t---- \t\t --------\n");
+	printf("\t\t\t\tName \t\t Quantity\n");
+	printf("\t\t\t\t---- \t\t --------\n\n");
+	for(i=0;i<n;i++){
+		printf("\n\n\t\t\t\t%s \t\t %d",o[i].name,o[i].quantity);
 	}
-	getch();
-	  char ch;
-	  float price;
-	  printf("\n\n\tDo you want to print Bill :");
-	  printf("\n\n\tEnter 'y' for yes and 'n' for NO ::");
-	  scanf("%c",&ch);
-     if(ch=='y'|| ch=='Y'){
-     	system("cls");
-     	printf("\n\n Enter the price of One gift :");
-     	scanf("%f",&price);
-     	system("cls");
-     	printf("\n\n Bill of every customer In order are ::");
-     	printf("\n Name \t\t Quantity \t Price ");
-     	
-     	for(i=-1;i<15;i++)   //for vertical lines
-		{  system("color A0");
-			gotoxy(38,1+i);printf("%c",178);// 1st vertical 
-			gotoxy(39,1+i);printf("%c",178);
-			gotoxy(81,1+i);printf("%c",178); // 2nd vertical 
-	        gotoxy(82,1+i);printf("%c",178);
-		}
-		  gotoxy(43,1);printf("\n\n Bill of every customer In order are ::");
-     	gotoxy(43,3);printf("\n Name \t\t Quantity \t Price ");
-     	
-     	for(i=0;i<10;i++){
-     		gotoxy(43,1+i);printf("%s",o[i].name);
-     		gotoxy(58,1+i);printf("%d",o[i].quantity);
-     		gotoxy(73,1+i);printf("%.2f",o[i].quantity*price);
-     	 }
-		 //system("cls");
-	 	printf("\n\n Thanks for using my Program : \n\n\t\t Have a Great Day !!");
-	 	getch();
-	 }
-	 else{
-	 	system("cls");
-	 	printf("\n\n Thanks for using my Program : \n\n\t\t Have a Great Day !!");
-	 	getch();
-	 }
-	 getch();
+	printf("\n");
+	sleep(3);
+	
+	// calling print function with price 
+	
+	printBill(o,n);
+	
+	// 
 }
 
+#define KYEL  "\x1B[33m"
+   
+// driver function to test the progam 
+   
+int main(){
+	printf("\e[38;5;196m Foreground color: red\n");
+	printf("%syellow\n", KYEL);
+	int st;
+	do{
+	system("clear");
+	printf("\n\n 1. Enter the program \n\n 2. Exit\n\n \t\t Enter your choice :: ->");
+	scanf("%d",&st);
+	switch(st){
+		case 1: program();
+				break;
+		case 2:exit(0);
+				break;
+		default:printf("\n You have entered wrong choice\n\n Press any key to continue ::\n\n ");		
+	}
+	}while(st!=2);
 
+}
 
